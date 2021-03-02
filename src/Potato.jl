55c98580dc779b4554af8c0ece3899abb4685231
kreikrei <kreitonstrs@gmail.com>
2021-03-02 16:49:23 +0700

    add base (extract! and datasets) and core (empty)

--- src/Potato.jl
+++ src/Potato.jl
@@ -1,5 +1,17 @@
 module Potato
 
-# Write your package code here.
+using JuMP
+using XLSX
+using DataFrames
+using Distances
+using Coluna
+using GLPK
+
+include("base.jl")
+include("core.jl")
+
+#base
+export extract!
+export V,dist,K,T,d
 
 end
